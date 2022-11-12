5004ad26b6c042e400e9760803bc4822eebf770a
akofisher <akolekveishvili@gmail.com>
2022-11-12 18:57:18 +0400

    first commit

--- src/ChuckIsReady.js
+++ src/ChuckIsReady.js
@@ -2,7 +2,7 @@ import { useState, useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import './index.css';
-import { delJokes, setCategory, setJokes } from './store/Chuck/ChuckActCreat';
+import { setCategory, setJokes } from './store/Chuck/ChuckActCreat';
 import { selectCategory, selectJokes } from './store/Chuck/ChuckSelector';
 
 function ChuckIsReady() {
@@ -13,38 +13,43 @@ function ChuckIsReady() {
     const [choosen, setChoosen] = useState(null)
     const [JokeCounter, setJokeCounter] = useState(1)
     const [realTimeJoke, setRealTimeJoke] = useState(null)
-    const [nextJoke, setNextJoke] = useState(0)
+    const [rand, setRand] = useState(false)
   
 
 
-    useEffect(() => {
-    }, [CATEGORIES])
+  
 
     useEffect(() => {
 
        console.log(CATJOKES, 'JOKES ARRAY')
+       //აქედან ჩანს რომ ერეის ვზრდი ხუმრობების მატების მიხედვით, კონკრეტულ სეგმენტზე დაჭერითაც და Next 
+       //დაჭერის შემთხვევაშიც რითაც ახალ რენდომ ხუმრობას ვტვირთავ ბოლო კატეგორიიდან
+       //შესაბამისად ერეისაც ვზრდი იმისთვის რო სრული ნანახი დატის გადათვალიერება იყოს შესაძლებელი
       }, [CATJOKES])
 
       useEffect(() => {
         setRealTimeJoke(CATJOKES.at(-JokeCounter))
+        //აქედან ვსეტავ ახლანდელ დროში რეალურად წამოღებულ ბოლო ხუმრობას ყოველი კატეგორიის არჩევაზე
+        //გამოყენებულია ერეის -1 პრინციპი რითიც ერეიში მყოფი ინდექსით ბოლოს ჩამატებულ დატას ვიღებთ
       }, [choosen])
 
       useEffect(() => {
          if (choosen !== null) {
              setRealTimeJoke(CATJOKES.at(-JokeCounter))
          }
+         //აქედან ვსეტავ ისევ რეალურ დროში ბოლოს წამოღებულ ხუმრობას ოღონდ იმ შემთხვევაში თუ კატეგორია არჩეული არ არის
+        //გამოყენებულია ერეის -1 პრინციპი რითიც ერეიში მყოფი ინდექსით ბოლოს ჩამატებულ დატას ვიღებთ
+
       }, [JokeCounter])
 
       useEffect(() => {
-          if(realTimeJoke !== undefined && realTimeJoke !== null){
-
-              console.log(realTimeJoke.value, 'EGIA JOUQI')
-          } else {
+          if(realTimeJoke == undefined || realTimeJoke == null || realTimeJoke == NaN){
               setChoosen(null)
-             
+          } 
 
-              console.log('euf')
-          }
+          //აქედან ვსეტავ კატეგორია არჩეული 'არაა'-ს  იმ დროს როცა Prev გადასვლით დატა სრულდება
+          //და კატეგორიის მონიშვნას ვხსნი და რენდომ ხუმრობას ვტვირთავ მხოლოდ
+          
       }, [realTimeJoke])
       
       
@@ -60,6 +65,7 @@ function ChuckIsReady() {
       dispatch(setCategory(json))
   }
     )
+    //აქედან კატეგორიებს ვსეტავ რედაქსში
     }, [])
 
     useEffect(() => {
@@ -69,28 +75,47 @@ function ChuckIsReady() {
         setRandomJoke(json.value)
     }
       )
-      }, [CATJOKES, nextJoke])
+      //აქედან რენდომ ხუმრობას ვსეტავ ჰუკში
+      }, [CATJOKES, rand])
 
 
       const SelectCat = (val, idx) => {
-        fetch(`https://api.chucknorris.io/jokes/random?category=${val}`)
-        .then(response => response.json())
-        .then(json => {
-            console.log(json, 'val')
-            setChoosen(idx)
-            dispatch(setJokes(json
-            ))
-            if(choosen == idx) {
-                setChoosen(null)
-               console.log('neta aq ara')
-
-                
-
+          if(val ) {
+            fetch(`https://api.chucknorris.io/jokes/random?category=${val}`)
+            .then(response => response.json())
+            .then(json => {
+                setChoosen(idx)
+                dispatch(setJokes(json
+                ))
+                if(choosen == idx) {
+                    setChoosen(null)
+                }
             }
-        }
-          )
+              )
+
+          } else  {
+              let CATEGOR = CATJOKES.at(-1)
+
+            fetch(`https://api.chucknorris.io/jokes/random?category=${CATEGOR.categories[0]}`)
+            .then(response => response.json())
+            .then(json => {
+                setChoosen(choosen)
+                dispatch(setJokes(json
+                ))
+            }
+              )
+          }
+
+          //ესაა ფუნქცია რომელიც ტვირთავს ხუმრობებს კატეგორიის მიხედვით და ეშვება ორ შემთხვევაში
+          //პირველი შემთხვევა არის როცა უბრალოდ კატეგორიას ვირჩევთ იტვირთება შესაბამისი რენდომ ხუმრობა
+          //მეორე შემთხვევა არის ის როცა Next-ით გადავდივართ შემდეგ ხუმრობაზე მაგრამ დატა აღარ გვაქვს,
+          //ამ დროს ვიყენებთ ჰუკებში შენახულ ინფორმაციას (კატეგორიის სახელს და ინდექსს) იმისთვის რო კატეგორიის შედარება
+          //და შესაბამისი რენდომ ხუმრობის წამოღება ხელახლა მოხდეს
+       
 
       }
+
+      
 
 
     
@@ -108,21 +133,20 @@ function ChuckIsReady() {
       </div>
       <div className='bodY'>
        <div className='Btns'>
-         <button onClick={() => {
-                console.log(JokeCounter, 'COUNT')
-                setJokeCounter(JokeCounter + 1)
-             
+         <button onClick={() => {    
+                setJokeCounter(JokeCounter + 1)   
+                //Prev შემთხვევაში +1 გასაკვირია მაგრამ ერეის -1 პრინციპის მიხედვით ასე ვალაგებ ინდექსებს
          }
             } className='BTN'>Prev</button>
          <button onClick={() => {
-                if (JokeCounter -  1 == 0){
-                    
-                   
+                if (choosen == null) {
+                    setRand(true)
+                    if(rand == true)
+                    setRand(false)
                 } else {
-                    console.log(JokeCounter, 'QOUNT +')
                     setJokeCounter(JokeCounter - 1)
-                }
-             
+                    SelectCat()
+                }     
         }
             } className='BTN'>Next</button>
        </div>
@@ -130,6 +154,7 @@ function ChuckIsReady() {
         <h4 className='JokeTXT'>
             {choosen !== null && realTimeJoke !== null && realTimeJoke !== undefined ? (realTimeJoke.value) : (randomJoke)
             }
+            {/* ეს ლოგიკაა რის მიხედვითაც გამოგვაქვს რენდომ ხუმრობა ან კატეგორიის რენდომ ხუმრობა */}
         </h4>
        </div>
        <div className='Categories'>
@@ -140,14 +165,12 @@ function ChuckIsReady() {
          {CATEGORIES.map((data, idx) => {
              if (idx < 5)
              return (
-                 <li onClick={() => SelectCat(data, idx)} key={idx} className={idx == choosen ? ('CatTitle1') : ('CatTitle')}
-                 
-                 >{data}</li>
-                 
+                 <li onClick={() => SelectCat(data, idx)} key={idx} className={idx == choosen ? ('CatTitle1') : ('CatTitle')}>
+                  {/* კლას ნეიმში მცირე ლოგიკაა რის მიხედვითაც ვადარებ აიდებს და ვიღებ გასამწვანებელ კატეგორიას */}
+                     {data}
+                     </li>
                  )
-                 
-                 
-                 
+
                 })
             }
             </ul>
